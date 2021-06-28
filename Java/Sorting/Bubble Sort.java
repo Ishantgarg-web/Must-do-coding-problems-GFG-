0b27fbe306d5d0ef48fe53591a73a81fa679da31
Ishantgarg-web <57170623+Ishantgarg-web@users.noreply.github.com>
2021-06-28 18:07:26 +0530

    Update Bubble Sort.java

--- Java/Sorting/Bubble Sort.java
+++ Java/Sorting/Bubble Sort.java
@@ -1,3 +1,5 @@
+//Naice Solution
+
 class Solution
 {
     //Function to sort the array using bubble sort algorithm.
@@ -18,6 +20,40 @@ class Solution
                 {
                     swap(arr,j-1,j);
                 }
+            }
+        }
+    }
+}
+
+//Optimised solution
+
+class Solution
+{
+    //Function to sort the array using bubble sort algorithm.
+    public static void swap(int a[],int i,int j)
+    {
+        int temp=a[i];
+        a[i]=a[j];
+        a[j]=temp;
+    }
+	public static void bubbleSort(int arr[], int n)
+    {
+        //code here
+        boolean swapeed=false;
+        for (int i=0;i<n-1;i++)
+        {
+            swapeed=false;
+            for (int j=1;j<=n-1-i;j++)
+            {
+                if(arr[j-1]>arr[j])
+                {
+                    swap(arr,j-1,j);
+                    swapeed=true;
+                }
+            }
+            if(swapeed==false)
+            {
+                return;
             }
         }
     }
